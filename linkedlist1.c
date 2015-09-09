9f61dee2024eb7bde05127f4e86ffb58f783fb98
sammy-iiitb <samyak.upadhyay123@gmail.com>
2015-09-09 22:41:54 +0530

    add more functionalities single linked list

--- linkedlist1.c
+++ linkedlist1.c
@@ -90,18 +90,94 @@ int linked_nth(struct linkedl* head, int index){
 	return head->value;
 }
 
+// Given only a pointer to a node to be deleted in a singly linked list, how do you delete it?
+
+void linked_delete_ref(struct linkedl *node_delete){
+	struct linkedl *node = node_delete;
+	struct linkedl *node_next = node_delete->next;
+	node->next = node_next->next;
+	node->value = node_next->value;
+	free(node_next);
+}
+
+// middle of a given linked list catch here is length of the linked list is takes as odd because 
+// if there are even elements then there will be two middle nodes
+
+void linked_middle(struct linkedl *head){
+	struct linkedl *node_2 = head->next;
+	struct linkedl *node_1 = head;
+	while(node_2 != NULL && node_2->next != NULL){
+		node_1 = node_1->next;
+		node_2 = node_2->next->next;
+	}
+	printf("%d\n", node_1->value);
+}
+
+// nth element from last
+
+void linked_nth_last(struct linkedl *head, int n){
+	struct linkedl *node_1 = head;
+	struct linkedl *node_2 = head;
+	while (n != 1){
+		node_1 = node_1->next;
+		n = n - 1;
+	}
+	while (node_1->next != NULL){
+		node_1 = node_1->next;
+		node_2 = node_2->next;
+	}
+	printf("%d\n", node_2->value);
+}
+
+// delete a linked list
+
+void linked_delete(struct linkedl *head){
+
+}
+
+// counts the number of times a given int occurs in a Linked List
+
+int linked_repeat(struct linkedl *head, int key){
+	struct linkedl *node = head;
+	int count = 0;
+	while (node != NULL){
+		if (node->value == key){
+			count = count + 1;
+		}
+		node = node->next;
+	}
+	printf("%d\n", count);
+	return count;
+}
+
+// reverseing a single linked list
+
+void linked_reverse(struct linkedl **head_ref){
+}
+
+// delete a node in a single linked list
+
+void delete_node(struct linkedl **head_ref){
+	
+}
+
 int main()
 {
   struct linkedl* head = NULL;
   add_front(&head, 6);
   add_front(&head, 7);
-  add_front(&head, 1);
-  add_last(&head, 4);
+  add_front(&head, 5);
+  add_last(&head, 5);
+  add_last(&head, 5);
   add_middle(head->next, 8);
-  printf("\n Created Linked list is: ");
+  //printf("\n Created Linked list is: \n");
   printList(head);
-  linked_length(head);
-  linked_search(head, 10);
-  linked_nth(head, 3);
+  printf("\n");
+  //linked_length(head);
+  //linked_search(head, 10);
+  //linked_nth(head, 3);
+  //linked_middle(head);
+  //linked_nth_last(head, 5);
+  linked_repeat(head, 1);
   return 0;
 }
