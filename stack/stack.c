db8508526dd58d96c9a7c5389e7a5bf5b07cbc1b
sammy-iiitb <samyak.upadhyay123@gmail.com>
2015-09-19 17:02:12 +0530

    fixed bugs array implementation

--- stack/stack.c
+++ stack/stack.c
@@ -11,18 +11,18 @@ struct stack{
 
 // create a stack of a given size
 
-struct stack* _stack(int _size){
+struct stack* create_stack(int _size){
 	struct stack* _stack = (struct stack*)malloc(sizeof(struct stack));
 	_stack->top = -1;
 	_stack->size = _size;
-	_stack->array = (int *)malloc(stack->size * sizeof(int));
+	_stack->array = (int *)malloc(_stack->size * sizeof(int));
 	return _stack;
 }
 
 // basic applications
 
 int isFull(struct stack* _stack){
-   if (_stack->top == _stack->capacity - 1){
+   if (_stack->top == _stack->size- 1){
 		return 1;
 	}
 	return 0;
@@ -45,12 +45,13 @@ void push(struct stack* _stack, int value){
 	}
 }
 
-void pop(struct stack* _stack){
+int pop(struct stack* _stack){
 	if (isEmpty(_stack)){
 		printf("stack is empty");
 		return 0;
 	}
 	_stack->top = _stack->top - 1;
+	return _stack->array[_stack->top+1];
 }
 
 void stack_delete(struct stack* _stack){
@@ -60,4 +61,19 @@ void stack_delete(struct stack* _stack){
 		}
 		free(_stack);
 	}
+}
+
+int main(){
+    struct stack* _stack = create_stack(100);
+ 
+    push(_stack, 10);
+    push(_stack, 20);
+    push(_stack, 30);
+ 
+    printf("%d popped from stack\n", pop(_stack));
+ 	printf("%d popped from stack\n", pop(_stack));
+ 	printf("%d popped from stack\n", pop(_stack));
+   // printf("Top item is %d\n", peek(stack));
+ 
+    return 0;
 }
